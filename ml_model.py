cffa9b8066daa64a7bc4b135e19b85b8af66a645
abhinavsp75 <abhinavsp@gmail.com>
2021-11-07 18:30:55 +0530

    added three new models

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,12 @@
 
 
 print "all the code for ML model will be added here"
+
+def model1():
+    print("adding code for ML model 1")
+
+def model2():
+    print("adding code for ML model 2")
+
+def model3():
+    print("adding code for ML model 3")
